4cfb8ebbc4bb4f010919537e8debbc82f3cbd611
BulinAlex <58906592+BulinAlex@users.noreply.github.com>
2019-12-16 05:46:20 +0300

    Add files via upload

---
+++ Lab6/src/BulinLab6/TesterDish.java
@@ -0,0 +1,8 @@
+package BulinLab6;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("фарфоровая", "синий", "огромная");
+        System.out.println(t);
+    }
+}
